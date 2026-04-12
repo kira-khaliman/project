8d8bdeaa44e898ca82e8a52c7130c4ee7d19fcc6
olga-oss-ss5 <temp.oos@yandex.ru>
2026-04-12 15:35:17 +0300

    создание интерфейса 2d фигур и класса окружности

---
+++ src/circle.cpp
@@ -0,0 +1,31 @@
+#include "Circle.hpp"
+#include <cmath>
+#include <iostream>
+
+Circle::Circle(double r): radius(r){
+	if (radius < 0){
+		std::cerr << "Ошибка. Отрицательного радиуса не существует" << std::endl;
+		radius = 0;
+	}
+}
+
+double Circle::getArea() const{
+	return M_PI * radius * radius;
+}
+
+double Circle::getCircumference() const{
+	return 2 * M_PI * radius;
+}
+
+double Circle::getRadius() const{
+	return radius;
+}
+
+void Circle::setRadius(double r){
+	if (r >= 0){
+		radius = r;
+	}
+	else{
+		std::cerr << "Ошибка. Отрицательного радиуса не существует" << std::endl;
+	}
+}
