699f53220bc5dd3ae39d7c3890ef844549dc4de4
olga-oss-ss5 <temp.oos@yandex.ru>
2026-04-12 16:00:29 +0300

    создание интерфейса 2d фигур и класса окружности

---
+++ src/triangle.cpp
@@ -0,0 +1,19 @@
+#include "triangle.h"
+#include <cmath> 
+Triangle::Triangle(double a, double b, double c) : a(a), b(b), c(c) {}
+
+bool Triangle::isValid() const {
+    return (a + b > c) && (a + c > b) && (b + c > a);
+}
+
+double Triangle::getArea() const {
+    if (!isValid()) {
+        return 0.0; 
+    }
+    double p = getPerimeter() / 2.0; 
+    return sqrt(p * (p - a) * (p - b) * (p - c)); 
+}
+
+double Triangle::getPerimeter() const {
+    return a + b + c;
+}
